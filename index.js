e8aad7b3e8b9083c1182d0e4af84afabb32b0498
Jeetendra-Choudhary <mailme@jeetendra.xyz>
2018-04-23 08:34:17 +0530

    Updated syntax

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const mime = require('mime');
 const through2 = require('through2');
 const fs = require('fs');
 
-function toFile(input, path) {
+toFile = (input, path) => {
 	return new Promise((resolve, reject) => {
 		if (typeof input !== 'string') {
 			return reject(new TypeError('Input should be string as base64'));
 		}
 
-		var data = re().exec(input);
+		let data = re().exec(input);
 		if (data) {
-			fs.writeFile(path + '.' + mime.extension(data[2]), data[4], {
+			let extension = mime.getExtension(data[2])
+			fs.writeFile(path + '.' + extension, data[4], {
 				encoding: "base64",
 				flag: "w"
 			}, (err, done) => {
@@ -24,8 +25,8 @@ function toFile(input, path) {
 					mimeType: data[2],
 					encoding: data[3],
 					data: new Buffer(data[4]),
-					path: path + '.' + mime.extension(data[2]),
-					extension: mime.extension(data[2])
+					path: path + '.' + extension,
+					extension: extension
 				} : undefined);
 			})
 		}
@@ -36,8 +37,8 @@ function toFile(input, path) {
 module.exports = toFile;
 
 module.exports.stream = () => {
-	return through2.obj(function(chunk, enc, cb) {
-		var _stream = this;
+	return through2.obj((chunk, enc, cb) => {
+		let _stream = this;
 		toFile(chunk.toString()).then(image => {
 			_stream.push(image.data);
 			cb();
